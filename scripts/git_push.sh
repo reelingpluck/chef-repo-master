b0340de9b4958fbdd808e9482771429d0f5aecdb
srinivas <srinivas.gmni@gmail.com>
2016-07-16 02:52:28 -0400

    adding script for git push

---
+++ scripts/git_push.sh
@@ -0,0 +1,12 @@
+#echo "############################################"
+echo -e "\e[1;31mPushing the json files to git repo\e[0m"
+git status
+git add $WORKSPACE/scripts/emails.sh $WORKSPACE/data_bags/users/$username.json $WORKSPACE/data_bags/private_keys/$username.json
+git commit -m "Uploading the users and private_keys json files of user $username"
+git push origin test
+
+if [ "$?" -eq 0 ]; then
+echo -e "\e[1;31mFiles are succesfully uploaded to git repo"
+else
+echo -e "\e[1;31mWe are facing an issue while uploadin files to git repo\e[0m"
+fi
