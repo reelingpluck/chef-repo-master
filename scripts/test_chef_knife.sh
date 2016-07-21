daf1b07ba2a83e1944088b2b2eb42607a1673349
srinivas <srinivas.gmni@gmail.com>
2016-07-21 18:50:29 +0530

    adding the chef script to execute the knife commands

--- scripts/test_chef_knife.sh
+++ scripts/test_chef_knife.sh
@@ -3,7 +3,7 @@
 rm -rf $WORKSPACE/scripts/mail_content.sh
 
 echo "###########################################"
-echo -e "\e[1;31mCreating the databag items for the $usernmae\e[0m"
+echo -e "\e[1;31mCreating the databag items for the $username\e[0m"
 knife ssl fetch
 sleep 5
 
@@ -24,7 +24,7 @@ fi
 
 if [ -f $WORKSPACE/data_bags/private_keys/$username.json ]; then
 echo -e "\e[1;31m Creating the private_key data bag item for $username\e[0m"
-knife vault create private_keys $username -A vakkavamsi -M client -S "name:my-chef-node" -j $WORKSPACE/data_bags/users/$username.json
+knife vault create private_keys $username -A vakkavamsi -M client -S "name:my-chef-node" -j $WORKSPACE/data_bags/private_keys/$username.json
 else
 echo -e "\e[1;31mPrivate_key json file is not found, please recheck\e[0m"
 exit 1
