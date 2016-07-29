498374a3249ac6ca52553d260527b52b7b69cd49
srinivas <srinivas.gmni@gmail.com>
2016-07-29 09:28:54 -0400

    adding new scripts

---
+++ scripts/git_push.sh
@@ -0,0 +1,17 @@
+#echo "############################################"
+echo -e "\e[1;31mPushing the json files to git repo\e[0m"
+git checkout test
+git status
+git add .
+git commit -m "Uploading the users and private_keys json files of user $username"
+git pull origin test
+git push origin test
+
+if [ "$?" -eq 0 ]; then
+git fetch && git log --name-status | grep $username
+git fetch && git log --name-status | grep -ir ops_users
+echo -e "\e[1;31mFiles are succesfully uploaded to git repo"
+else
+echo -e "\e[1;31mWe are facing an issue while uploading files to git repo\e[0m"
+exit 1
+fi
