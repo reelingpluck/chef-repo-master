0c94aa7d987d8db95862df41cb77e8343695df93
srinivas <srinivas.gmni@gmail.com>
2016-07-29 11:03:31 -0400

    making changes to git

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -9,7 +9,7 @@ git push origin test
 
 if [ "$?" -eq 0 ]; then
 git fetch && git log --name-status | grep $username
-git fetch && git log --name-status | grep  ops_users
+git fetch && git log --name-status | grep  ops_users | head -3
 echo -e "\e[1;31mFiles are succesfully uploaded to git repo"
 else
 echo -e "\e[1;31mWe are facing an issue while uploading files to git repo\e[0m"
