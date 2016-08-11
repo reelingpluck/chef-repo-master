9c51cb1bc952c0e92addd945e46b32c30a51e22c
srinivas <srinivas.gmni@gmail.com>
2016-08-10 22:31:29 -0400

    making some changes to chef file

--- scripts/test_chef_knife.sh
+++ scripts/test_chef_knife.sh
@@ -25,7 +25,7 @@ fi
 
 if [ -f data_bags/private_keys/ops_users.json ]; then
 echo -e "\e[1;31m Updating the ops_users file for user $username\e[0m"
-knife vault update private_keys ops_users -A vakkavamsi -M client -S "os:linux" -j data_bags/private_keys/ops_users.json
+knife vault update private_keys ops_users -A ngupta -M client -S "os:linux" -j data_bags/private_keys/ops_users.json
 else
 echo -e "\e[1;31mPrivate_key json file is not found, please recheck\e[0m"
 exit 1
