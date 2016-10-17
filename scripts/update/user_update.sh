7f4c632750b47d176c6e4b01f46aa00bbc8552fe
srinivas <srinivas.gmni@gmail.com>
2016-10-17 06:08:47 -0700

    hello

--- scripts/update/user_update.sh
+++ scripts/update/user_update.sh
@@ -87,4 +87,18 @@ sh update/ops_key_update.sh
 fi
 
 
-
+if [ "$password" != "" ];then
+echo -e "\e[1;32mGoing to update the password\e[0m"
+cp -rf data_bags/users/$username.json back_up/$username.json.bak
+sed -i '/"action"/c\  "action": "update"' data_bags/users/$username.json
+jsonlint data_bags/users/$username.json
+if [ $? -eq 0 ]; then
+echo -e "\e[1;32mUser file is in valid json format\e[0m"
+else
+echo -e "\e[1;31mFile is not in valid json format, please check\e[0m"
+rm -rf data_bags/users/$username.json
+cp -rf back_up/$username.json.bak data_bags/users/$username.json
+exit 1
+fi
+sh update/password_update.sh
+fi
